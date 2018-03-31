e0660da249b69262116bf1720b0f1c60ecb5558d
Christopher Hopper <ch.hopper@gmail.com>
2018-03-31 02:01:11 +0200

    added (first) emacs config

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,37 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Make sure we have use-package enabled
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+      (package-install 'use-package))
+
+(eval-when-compile
+    (require 'use-package))
+
+(setq use-package-always-ensure t) ;; always download missing packages
+(setq use-package-always-demand t) ;; always load packages on startup
+
+;; Packages
+(use-package evil)
+(use-package helm)
+(use-package magit)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit helm evil-mode use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
